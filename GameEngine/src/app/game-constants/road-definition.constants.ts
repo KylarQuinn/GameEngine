487dc6279f204d5de11f8603f5fc875672a0377e
Ethan Lee <ethan.lee@jacks.sdstate.edu>
2019-03-17 12:42:21 -0600

    remove whitespace and unused tr

--- GameEngine/src/app/game-constants/road-definition.constants.ts
+++ GameEngine/src/app/game-constants/road-definition.constants.ts
@@ -4,7 +4,6 @@ export class Road {
   // Todo:  Think about reformatting.
   public static SouthRoad: Array<number> = [
     4975, 4950, 4951, 4952, 4953, 4954, 4955, 4956, 4957, 4958, 4959, 4960, 4961, 4962
-
   ];
 
   public static contains(road: Array<number>, rowIndex: number, columnIndex: number): boolean {
